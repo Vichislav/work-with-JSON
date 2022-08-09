9e35e860811d01538655f981bc1cef229d998844
Svalyan <you@example.com>
2022-08-09 17:12:03 +0600

    map and filter and if  work

--- search.js
+++ search.js
@@ -1,6 +1,6 @@
-const sberData = require('./sber.json');
+const sberData = require('./sber.json');//запрашиваем json
 
-const Data = '04-12-2019';
+const sDate = '04-12-2019'; // Date с чем сравниваем (эталон)
 
 function getMilliSecTime(data) {
     let a = data.split('-').reverse(); //превратили в массив и развернули
@@ -9,16 +9,21 @@ function getMilliSecTime(data) {
     return c;
 }
 
-let arr = sberData.CardInfo
-    .map(card => {
-    currentData = getMilliSecTime(card.ExpDate);
-    return {
-        "CardNumber": card.CardNumber,
-        "CardName": card.CardName,
-        "ExpDate": currentData
-    }
-})
-    .filter(card => card.ExpDate < getMilliSecTime(Data))
-    .map(card => card.CardNumber.substr(12, 4))
-
-console.log(arr);
+function getCardsArrayExpDate(json, date) {
+    let Date = getMilliSecTime(date) // перевод даты (эталона) в нужный вид
+    let arr = json.CardInfo
+        .map(card => {
+            currentData = getMilliSecTime(card.ExpDate); //настраиваем дату в сравняемый формат
+            return {
+                "CardNumber": card.CardNumber,
+                "CardName": card.CardName,
+                "ExpDate": currentData // помещаем дату в новый массив
+            }
+        })
+        .filter(card => card.ExpDate < Date) //сравниваем ее с нужной датой, лишнее фильтруем
+        .map(card => card.CardNumber.substr(12, 4)) // создаем новый массив только из фрагментов № карт
+    if(arr.length == 0)  return null;
+    else return arr; // проверка на пустой массив
+};
+//часть задания с некорректными входными данными не совсем понял как реализовать, увы
+    console.log(getCardsArrayExpDate(sberData, sDate)); //проверка
